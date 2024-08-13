d4a455e0c6a2ed9de128803a493bbea18ea0e69a
DESKTOP-04M5OI4\DulanjayaSandaruwan <dulanjayasandaruwan1998@gmail.com>
2024-08-13 10:32:45 +0530

    add some configs

--- src/main/java/com/project/abc/service/user/UserService.java
+++ src/main/java/com/project/abc/service/user/UserService.java
@@ -24,7 +24,6 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-
     public User createUser(UserDTO dto) {
         log.info("create user email {}", dto.getEmail());
         User user = User.init(dto);
