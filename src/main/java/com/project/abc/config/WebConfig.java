57ac54fb14cd73a5a459bd27c9cfc9f22af9e3f0
DESKTOP-04M5OI4\DulanjayaSandaruwan <dulanjayasandaruwan1998@gmail.com>
2024-08-18 21:37:20 +0530

    remove unused imports

--- src/main/java/com/project/abc/config/WebConfig.java
+++ src/main/java/com/project/abc/config/WebConfig.java
@@ -1,8 +1,5 @@
 package com.project.abc.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
